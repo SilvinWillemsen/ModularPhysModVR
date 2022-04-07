89abb42da8d9ef5799426c0a50706f82131f459a
SilvinWillemsen <silvinwillemsen@gmail.com>
2022-04-07 16:46:15 +0200

    Working on

--- Assets/Scripts/SetCameraRig.cs
+++ Assets/Scripts/SetCameraRig.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using Tilia.CameraRigs.TrackedAlias;
 using Zinnia.Tracking.CameraRig;
+using Tilia.Interactions.Interactables.Interactors;
+using Zinnia.Action;
+using System.Linq;
+using UnityEditor;
 
 [ExecuteInEditMode]
 public class SetCameraRig : MonoBehaviour
@@ -16,11 +20,17 @@ public class SetCameraRig : MonoBehaviour
     public XRsetup xrSetup;
     private XRsetup curXRSetup;
 
-    public GameObject TrackedAlias;
+    public GameObject TrackedAliasObj;
     public LinkedAliasAssociationCollection OculusCameraRig;
     public LinkedAliasAssociationCollection SimulatorCameraRig;
-    public GameObject LeftInteractor;
-    public GameObject RightInteractor;
+    public GameObject LeftInteractorObj;
+    public GameObject RightInteractorObj;
+
+    public BooleanAction OculusLeftGrip;
+    public BooleanAction OculusRightGrip;
+    public BooleanAction SimulatorLeftGrip;
+    public BooleanAction SimulatorRightGrip;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,10 +38,12 @@ public class SetCameraRig : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void OnValidate()
     {
+        Debug.Log("OnValidate");
         if (curXRSetup != xrSetup)
         {
+            //StartCoroutine (ChangeXRSettings());
             ChangeXRSettings();
             curXRSetup = xrSetup;
         }
@@ -42,14 +54,25 @@ public class SetCameraRig : MonoBehaviour
         bool switchToOculus = xrSetup == XRsetup.UseOculus;
         Debug.Log(xrSetup == XRsetup.UseOculus ? "Changing to Oculus" : "Changing to Simulator");
 
+        Debug.Log("Activating correct GameObject");
         // Activate the correct GameObject
         OculusCameraRig.gameObject.SetActive (switchToOculus);
         SimulatorCameraRig.gameObject.SetActive (!switchToOculus);
 
-        // Change the Tracked alias list
-        TrackedAlias.GetComponent<TrackedAliasFacade>().CameraRigs.Clear();
-        TrackedAlias.GetComponent<TrackedAliasFacade>().CameraRigs.Add(switchToOculus ? OculusCameraRig : SimulatorCameraRig);
+        Debug.Log("Change interactor grip settings");
 
-        // 
+        // Change interactor grip settings
+        LeftInteractorObj.GetComponent<InteractorFacade>().GrabAction = switchToOculus ? OculusLeftGrip : SimulatorLeftGrip;
+        RightInteractorObj.GetComponent<InteractorFacade>().GrabAction = switchToOculus ? OculusRightGrip : SimulatorRightGrip;
+
+
+        Debug.Log("Change the Tracked alias list");
+
+        var so = new SerializedObject(TrackedAliasObj);
+        so.FindProperty("m_LocalPosition")
+        // Change the Tracked alias list
+        TrackedAliasObj.GetComponent<TrackedAliasFacade>().CameraRigs.Clear();
+        TrackedAliasObj.GetComponent<TrackedAliasFacade>().CameraRigs.Add(switchToOculus ? OculusCameraRig : SimulatorCameraRig);
+
     }
 }
