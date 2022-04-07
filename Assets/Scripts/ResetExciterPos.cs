b9e053d93f40aefc03c58f0ddcd4dd3711c4e3a6
HNuijens <helmernuijens@gmail.com>
2022-04-07 16:31:22 +0200

    exciters now also despawn and spawn

---
+++ Assets/Scripts/ResetExciterPos.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetExciterPos : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    ExciterReferenceList exciterReferenceList;
+
+    [SerializeField] private float timeBeforeDespawn = 1.0f;
+    [SerializeField] private float despawnTime = 1.0f;
+    [SerializeField] private float spawnTime = 1.0f;
+    [SerializeField] private float transitionTime = 0.5f;
+
+    void Start()
+    {
+        exciterReferenceList = GetComponent<ExciterReferenceList>();
+    }
+    public void DespawnAndSpawnExciter(GameObject exciter)
+    {
+        List<GameObject> thisInstrument = new List<GameObject>();
+        thisInstrument.Add(exciter);
+        StartCoroutine(StartResetCoroutine(thisInstrument, despawnTime, spawnTime, transitionTime));
+    }
+
+    IEnumerator StartResetCoroutine(List<GameObject> thisInstrument, float despawnTime, float spawnTime, float transitionTime)
+    {
+        yield return new WaitForSeconds(timeBeforeDespawn);
+        Global.DespawnInstruments(thisInstrument, despawnTime, false);
+        yield return new WaitForSeconds(transitionTime + despawnTime); // wait for despawnTime + transition time before spawning agia
+        thisInstrument[0].transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.transform.localScale = Vector3.zero;
+        Global.SpawnInstruments(thisInstrument, spawnTime, exciterReferenceList.exciterStartPos, exciterReferenceList.exciterStartOrientation);
+    }
+}
