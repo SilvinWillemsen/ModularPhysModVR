d7b36ca685c908d4a8c22296ce98929a5c730e62
SilvinWillemsen <silvinwillemsen@gmail.com>
2022-04-07 17:47:49 +0200

    Change tracked alias from script seems to work now. Just changing toggle now

--- Assets/Scripts/SetCameraRig.cs
+++ Assets/Scripts/SetCameraRig.cs
@@ -23,7 +23,10 @@ public class SetCameraRig : MonoBehaviour
     public GameObject TrackedAliasObj;
     public LinkedAliasAssociationCollection OculusCameraRig;
     public LinkedAliasAssociationCollection SimulatorCameraRig;
+    
+    [SerializeField]
     public GameObject LeftInteractorObj;
+    [SerializeField]
     public GameObject RightInteractorObj;
 
     public BooleanAction OculusLeftGrip;
@@ -62,12 +65,28 @@ public class SetCameraRig : MonoBehaviour
         Debug.Log("Change interactor grip settings");
 
         // Change interactor grip settings
-        LeftInteractorObj.GetComponent<InteractorFacade>().GrabAction = switchToOculus ? OculusLeftGrip : SimulatorLeftGrip;
-        RightInteractorObj.GetComponent<InteractorFacade>().GrabAction = switchToOculus ? OculusRightGrip : SimulatorRightGrip;
+        var leftSO = new SerializedObject(LeftInteractorObj.GetComponent<InteractorFacade>());
+        var rightSO = new SerializedObject(RightInteractorObj.GetComponent<InteractorFacade>());
+
+        leftSO.FindProperty("grabAction").objectReferenceValue = switchToOculus ? OculusLeftGrip : SimulatorLeftGrip;
+        leftSO.ApplyModifiedProperties();
+
+        rightSO.FindProperty("grabAction").objectReferenceValue = switchToOculus ? OculusRightGrip : SimulatorRightGrip;
+        rightSO.ApplyModifiedProperties();
+
+        // LeftInteractorObj.GetComponent<InteractorFacade>().GrabAction = switchToOculus ? OculusLeftGrip : SimulatorLeftGrip;
+        // RightInteractorObj.GetComponent<InteractorFacade>().GrabAction = switchToOculus ? OculusRightGrip : SimulatorRightGrip;
 
 
         Debug.Log("Change the Tracked alias list");
         // Change the Tracked alias list
+        // var trackedAliasSO = new SerializedObject(TrackedAliasObj.GetComponent<TrackedAliasFacade>().CameraRigs);
+
+        // LinkedAliasAssociationCollectionObservableList listTest;
+        // listTest.Add(switchToOculus ? OculusCameraRig : SimulatorCameraRig);
+        // trackedAliasSO.FindProperty("elements.Array.data[0]").objectReferenceValue = switchToOculus ? OculusCameraRig : SimulatorCameraRig;
+
+        // trackedAliasSO.FindProperty("elements").objectReferenceValue = listTest;
         TrackedAliasObj.GetComponent<TrackedAliasFacade>().CameraRigs.Clear();
         TrackedAliasObj.GetComponent<TrackedAliasFacade>().CameraRigs.Add(switchToOculus ? OculusCameraRig : SimulatorCameraRig);
 
