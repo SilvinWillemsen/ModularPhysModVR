b91676fa90f74d3063de3e1df0683489292bb202
SilvinWillemsen <silvinwillemsen@gmail.com>
2022-04-08 09:11:39 +0200

    Now Grabaction change also works for the exciters!

--- Assets/Scripts/SetCameraRig.cs
+++ Assets/Scripts/SetCameraRig.cs
@@ -36,6 +36,7 @@ public class SetCameraRig : MonoBehaviour
     public BooleanAction SimulatorRightGrip;
 
     public GameObject instrumentDisplays;
+    public GameObject exciters;
 
     // Start is called before the first frame update
     void Start()
@@ -67,7 +68,7 @@ public class SetCameraRig : MonoBehaviour
 
         Debug.Log("Change interactor grip settings");
 
-        // Change interactor grip settings
+        // Change interactor grip settings using serializedobjects
         var leftSO = new SerializedObject(LeftInteractorObj.GetComponent<InteractorFacade>());
         var rightSO = new SerializedObject(RightInteractorObj.GetComponent<InteractorFacade>());
 
@@ -81,29 +82,31 @@ public class SetCameraRig : MonoBehaviour
         // RightInteractorObj.GetComponent<InteractorFacade>().GrabAction = switchToOculus ? OculusRightGrip : SimulatorRightGrip;
 
 
+        // Change the Tracked alias list (apparently no need for serializedobjects)
         Debug.Log("Change the Tracked alias list");
-        // Change the Tracked alias list
-        // var trackedAliasSO = new SerializedObject(TrackedAliasObj.GetComponent<TrackedAliasFacade>().CameraRigs);
 
-        // LinkedAliasAssociationCollectionObservableList listTest;
-        // listTest.Add(switchToOculus ? OculusCameraRig : SimulatorCameraRig);
-        // trackedAliasSO.FindProperty("elements.Array.data[0]").objectReferenceValue = switchToOculus ? OculusCameraRig : SimulatorCameraRig;
-
-        // trackedAliasSO.FindProperty("elements").objectReferenceValue = listTest;
         TrackedAliasObj.GetComponent<TrackedAliasFacade>().CameraRigs.Clear();
         TrackedAliasObj.GetComponent<TrackedAliasFacade>().CameraRigs.Add(switchToOculus ? OculusCameraRig : SimulatorCameraRig);
 
-        
+        // Change grabaction in instrument interactables
         foreach (Transform child in instrumentDisplays.transform)
         {
             if (child.GetChild(0).tag == "Instrument")
             {                
-                var toggleVar = Tilia.Interactions.Interactables.Interactables.Grab.Receiver.GrabInteractableReceiver.ActiveType.Toggle;
-                var holdTillReleaseVar = Tilia.Interactions.Interactables.Interactables.Grab.Receiver.GrabInteractableReceiver.ActiveType.HoldTillRelease;
-
                 var interactableSO = new SerializedObject(child.GetChild(0).GetComponent<InteractableFacade>());
-                interactableSO.FindProperty("grabType").enumValueIndex = switchToOculus ? 0 : 1;
-                Debug.Log(interactableSO.FindProperty("grabType").enumValueIndex);
+                interactableSO.FindProperty("grabType").enumValueIndex = switchToOculus ? 0 : 1; // 0: Hold Till Release, 1: Toggle
+                interactableSO.ApplyModifiedProperties();
+
+            }
+        }
+
+        foreach (Transform child in exciters.transform)
+        {
+            if (child.GetChild(0).tag == "Exciter")
+            {
+                Debug.Log("Changing to " + switchToOculus + " for " + child.GetChild(0).name);
+                var interactableSO = new SerializedObject(child.GetChild(0).GetComponent<InteractableFacade>());
+                interactableSO.FindProperty("grabType").enumValueIndex = switchToOculus ? 0 : 1; // 0: Hold Till Release, 1: Toggle
                 interactableSO.ApplyModifiedProperties();
 
             }
