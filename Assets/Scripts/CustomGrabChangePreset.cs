71b43499bc23336355d14c415c31d6a09cfd4b53
ME-Lab SiderumsPC <57089470+Aekp@users.noreply.github.com>
2022-04-29 16:05:11 +0200

    harp play area

--- Assets/Scripts/CustomGrabChangePreset.cs
+++ Assets/Scripts/CustomGrabChangePreset.cs
@@ -7,6 +7,7 @@ public class CustomGrabChangePreset : MonoBehaviour
     public void ChangePreset(Tilia.Interactions.Interactables.Interactables.Operation.Extraction.InteractableFacadeExtractor grabbedObjectExtractor)
     {
         GameObject extractedGameObject = grabbedObjectExtractor.Source.gameObject;
-        extractedGameObject.transform.GetChild(0).transform.GetChild(1).GetComponent<SelectPreset>().InstrumentGrabbed();
+        Transform instrumentChild = extractedGameObject.transform.GetChild(0).transform.GetChild(1);
+        if(instrumentChild.GetComponent<SelectPreset>() != null) instrumentChild.GetComponent<SelectPreset>().InstrumentGrabbed();
     }
 }
